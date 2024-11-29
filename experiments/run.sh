5e737e49ce7ab6eb29cac04718e210f56b710211
e-strauss <92718421+e-strauss@users.noreply.github.com>
2024-11-29 01:12:53 +0100

    store experiment out in file

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,10 +1,11 @@
 source parameters.sh
-
+logstart="results_algorithms/"
 x='kmeans+'
 folder="census"
 seed=3333
 d="census_enc_16k"
 y="ulab16"
+fullLogname="$logstart/$x/$d/$y-singlenode.log"
 #perf stat -d -d -d \
 systemds \
 code/algorithms/$x.dml \
@@ -18,5 +19,5 @@ code/algorithms/$x.dml \
 "data/$folder/train_${d}_labels.data" \
 "data/$folder/test_${folder}.data" \
 "data/$folder/test_${folder}_labels.data"
-#>>$fullLogname 2>&1
+>>$fullLogname 2>&1
 # -explain \
