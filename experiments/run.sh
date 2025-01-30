b6ea8b632a5f2e3d294a43ac1560e67cdeeb67ed
elias <elias.strauss@campus.tu-berlin.de>
2025-01-30 21:16:20 +0100

    new experiments

--- experiments/run.sh
+++ experiments/run.sh
@@ -6,20 +6,21 @@ seed=3333
 d="census_enc"
 y="claWorkloadb1"
 #y="cla16"
+#y="ula.xml"
 fullLogname="$logstart/$x/$d/$y-singlenode.log"
 mkdir -p "$(dirname "$fullLogname")"
-#perf stat -d -d -d \
-systemds \
-code/algorithms/$x.dml \
--config code/conf/$y.xml \
--stats 100 -debug \
--exec singlenode \
--seed $seed \
--args "data/$folder/train_$d.data" \
-1 \
-"results/algorithms/$x/$d/$y.csv" \
-"data/$folder/train_${d}_labels.data" \
-"data/$folder/test_${folder}.data" \
-"data/$folder/test_${folder}_labels.data" \
-> $fullLogname 2>&1
+perf stat -d -d -d \
+  systemds \
+  code/algorithms/$x.dml \
+  -config code/conf/$y.xml \
+  -stats 100 -debug \
+  -exec singlenode \
+  -seed $seed \
+  -args "data/$folder/train_$d.data" \
+  1 \
+  "results/algorithms/$x/$d/$y.csv" \
+  "data/$folder/train_${d}_labels.data" \
+  "data/$folder/test_${folder}.data" \
+  "data/$folder/test_${folder}_labels.data" \
+  > $fullLogname 2>&1
 # -explain \
--- experiments/setup_systemds.sh
+++ experiments/setup_systemds.sh
@@ -8,6 +8,8 @@ fi
 
 cd ~/github/systemds
 git fetch
+#systemDSID=
+#git checkout "c6195020bf19c9e50f091cfc120bd2ec1805c00e"
 git checkout -b compress_experiments origin/compress_experiments
 mvn clean package
 export SYSTEMDS_ROOT="$HOME/github/systemds"
